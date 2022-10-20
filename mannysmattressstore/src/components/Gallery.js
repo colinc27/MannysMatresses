b770e71e8643fcc7f4812720e2b8e4897924148f
colinc27 <colinchristoph27@gmail.com>
2022-10-19 18:49:01 -0700

    CardCommit

--- mannysmattressstore/src/components/Gallery.js
+++ mannysmattressstore/src/components/Gallery.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Card } from "react-bootstrap";
 import img from "../img";
 
 const Gallery = () => {
   return (
-    <Container className="">
+    <Container className="py-2 d-flex justify-content-center">
       <Row className="">
         {img.map((img) => (
-          <Col key={img._id} className="galImage">
-            <img className="galImage py-4" src={img.img} alt="mattress" />
+          <Col className=" py-2" lg={4}>
+            <Card key={img._id}>
+              <Card.Img variant="top" src={img.img} />
+              <Card.Body>
+                <Card.Title>{img.description}</Card.Title>
+                <Card.Text className="text-warning">{img.price}</Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
         ))}
       </Row>
